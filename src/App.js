02479a531d17029a97b0693a68465f0eb0fd9aa0
Aditya Vashisht <vash0005@algonquinlive.com>
2019-10-17 16:50:44 -0400

    added basename to the router in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   render () {
     return (
 
-      <Router>
+      <Router basename="/mad9135-c1-react-router">
         <Switch>
           <Route exact path="/" component={AppUsers} />
 
